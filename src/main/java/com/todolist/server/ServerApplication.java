0dfe4d08da0e3348de09c7237c71dc828710e383
Kim Young Wan <qodna25@gmail.com>
2024-09-11 13:17:21 +0900

    chore:initial setting

---
+++ src/main/java/com/todolist/server/ServerApplication.java
@@ -0,0 +1,25 @@
+package com.todolist.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@SpringBootApplication
+@RestController
+public class ServerApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s", name);
+    }
+
+}
